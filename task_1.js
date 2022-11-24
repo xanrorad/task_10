01a6696bef1c99839f46dc2b0f4c3ac92b19f97b
Andrey <andre2004991@gmail.com>
2022-11-25 00:29:41 +0200

    first commit

---
+++ task_1.js
@@ -0,0 +1,15 @@
+function mul() {  
+    var tmp = 1;   
+    var count = 0;  
+    for (var i = 0; i < arguments.length; i++){  
+        if (typeof(arguments[i])==="number"){  
+            tmp *=arguments[i];  
+            count++;  
+        }  
+    }  
+    if (count == 0) return 0;  
+        return tmp;  
+}  
+    
+console.log(mul(1, "str", 2, 3, true)); // 6  
+console.log(mul(null, "str", false, true)); // 0 
---
+++ task_2.js
@@ -0,0 +1,16 @@
+var country = {
+    name: "Ukraine",
+    language: "ukrainian",
+    capital: {
+        name: "Kyiv",
+        population: 2907817,
+        area: 847.66,
+    },
+};
+function format(beginMsg, endMsg) {
+    console.log(beginMsg + this.name + endMsg);
+}
+format.call(country, "<", ">"); // "<Ukraine>"
+format.apply(country, ["[", "]"]); // "[Ukraine]"
+format.call(country.capital, '"', '"'); // ""Kyiv""
+format.apply(country.capital, ["",""]); // "Kyiv"
